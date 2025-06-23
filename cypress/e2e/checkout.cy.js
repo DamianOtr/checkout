5dd626a38498761f41635d9fefd4b8cdc5104461
DamianOtr <d.otrebski@vasco-electronics.com>
2025-06-24 00:01:10 +0200

    test checkoutu

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -2,35 +2,24 @@
 
 // const { describe } = require("mocha");
 
-const translations = {
-    pl: {
-      step1: 'Wprowadź podstawowe dane',
-      step2: 'Jaki jest Twój adres do faktury?'
-    },
-    en: {
-      step1: 'Fill in basic information',
-      step2: 'What is your billing address?'
-    }
-  }
-  
   describe('Checkout', () => {
   
     beforeEach(() => {
-      cy.log('Przygotowanie: Dodanie produktu do koszyka i przejście do checkoutu');  // funkcja w commands
+      cy.log('Przygotowanie: Dodanie produktu do koszyka i przejście do checkoutu');
         cy.checkout()
       });
     
     it('Poprawne wypełnienie danych osobowych w pierwszym kroku', () => {
       cy.log('Sprawdzenie widoczności pierwszego kroku');
-      cy.sprawdzenie_kroku1()   // funkcja w commands
+      cy.sprawdzenie_kroku1() 
       cy.log('Wpisanie poprawnych danych');
-        cy.poprawne_dane1krok()   // funkcja w commands
+        cy.poprawne_dane1krok()
       cy.log('Zgody: regulamin i polityka prywatności');
-        cy.checkboxy()    // funkcja w commands
+        cy.checkboxy()
       cy.log('Kliknięcie buttona z przejściem dalej');
-      cy.button1_continue()    // funkcja w commands
+      cy.button1_continue()
       cy.log('Sprawdzenie, czy przeszedł do kroku "Adresy"');
-      cy.sprawdzenie_kroku2()   // funkcja w commands
+      cy.sprawdzenie_kroku2()
       });
   
       it('Niepoprawne imię', () => {
@@ -38,13 +27,13 @@ const translations = {
         cy.get('input[name="lastname"]').type('Testowy');
         cy.get('input[name="email"]').type('test@test.st');
       cy.log('Zaznaczenie zgód');
-      cy.checkboxy()    // funkcja w commands
+      cy.checkboxy()
       cy.log('Próba przejścia dalej');
-      cy.button1_continue()    // funkcja w commands
+      cy.button1_continue()
       cy.log('Sprawdzenie, czy zostaliśmy na pierwszym kroku');
-      cy.sprawdzenie_kroku1()   // funkcja w commands 
+      cy.sprawdzenie_kroku1()
       cy.log('Sprawdzenie, czy pojawia się błąd');
-      cy.sprawdzenie_error1krok()   // funkcja w commands
+      cy.sprawdzenie_error1krok()
       });
   
       it('Niepoprawne nazwisko', () => {
@@ -52,13 +41,13 @@ const translations = {
         cy.get('input[name="lastname"]').type('Testowy123');
         cy.get('input[name="email"]').type('test@test.st');
       cy.log('Zaznaczenie zgód');
-      cy.checkboxy()    // funkcja w commands
+      cy.checkboxy()
       cy.log('Próba przejścia dalej');
-      cy.button1_continue()    // funkcja w commands
+      cy.button1_continue()
       cy.log('Sprawdzenie, czy zostaliśmy na pierwszym kroku');
-      cy.sprawdzenie_kroku1()   // funkcja w commands
+      cy.sprawdzenie_kroku1()
       cy.log('Sprawdzenie, czy pojawia się błąd');
-      cy.sprawdzenie_error1krok()   // funkcja w commands
+      cy.sprawdzenie_error1krok()
       });
     
       it('Niepoprawny adres e-mail', () => {
@@ -66,60 +55,58 @@ const translations = {
         cy.get('input[name="lastname"]').type('Testowy');
         cy.get('input[name="email"]').type('testtest.st'); // brak @
       cy.log('Zaznaczenie zgód');
-      cy.checkboxy()    // funkcja w commands
+      cy.checkboxy()
       cy.log('Próba przejścia dalej');
-      cy.button1_continue()    // funkcja w commands
+      cy.button1_continue()
       cy.log('Sprawdzenie, czy zostaliśmy na pierwszym kroku');
-      cy.sprawdzenie_kroku1()   // funkcja w commands
+      cy.sprawdzenie_kroku1()
       cy.log('Sprawdzenie, czy pojawia się błąd');
-      cy.sprawdzenie_error1krok()   // funkcja w commands
+      cy.sprawdzenie_error1krok()
       });
     
       it('Brak zaznaczenia zgód', () => {
-        cy.poprawne_dane1krok()   // funkcja w commands
+        cy.poprawne_dane1krok()
       cy.log('Brak zaznaczenia zgód i próba przejścia dalej');
-      cy.button1_continue()    // funkcja w commands
+      cy.button1_continue()
         cy.log('Sprawdzenie, czy zostaliśmy na pierwszym kroku');
-        cy.sprawdzenie_kroku1()   // funkcja w commands
+        cy.sprawdzenie_kroku1()
       });
     
       it('Poprawne wypełnienie formularza Adresy', () => {
-        cy.poprawne_dane1krok()   // funkcja w commands
-        cy.checkboxy()    // funkcja w commands
-        cy.button1_continue()    // funkcja w commands
+        cy.poprawne_dane1krok()
+        cy.checkboxy()
+        cy.button1_continue()
       cy.log('Sprawdzenie, czy przeszło do kroku "Adresy"');
-      cy.sprawdzenie_kroku2()   // funkcja w commands
+      cy.sprawdzenie_kroku2()
       cy.log('Wprowadzanie poprawnych danych adresowych');
         cy.get('input[name="address1"]').type('Testowa 12');
         cy.get('input[name="postcode"]').type('0123');
         cy.get('input[name="city"]').type('Test');
         cy.get('input[name="phone"]').type('123123123');
       cy.log('Kliknięcie "Dalej"');
-      cy.button2_continue()    // funkcja w commands
+      cy.button2_continue()
       cy.log('Sprawdzenie, czy przeszło do kolejnego kroku z wyborem dostawy');
-      cy.sprawdzenie_kroku3()   // funkcja w commands
+      cy.sprawdzenie_kroku3()
     });
   
    // cy.get('#checkout-addresses-step').should('not.be.visible');    takie przejścia między krokami?
    // cy.get('#checkout-delivery-step').should('be.visible');
   
-  
-  
    // .type('{selectall}{del}')
   
     it('Niepoprawne imię w adresie (cyfry)', () => {
-        cy.poprawne_dane1krok()   // funkcja w commands
-        cy.checkboxy()    // funkcja w commands
-        cy.button1_continue()    // funkcja w commands
+        cy.poprawne_dane1krok()
+        cy.checkboxy()
+        cy.button1_continue()
       cy.log('Sprawdzenie, czy przeszło do kroku "Adresy"');
-      cy.sprawdzenie_kroku2()   // funkcja w commands
+      cy.sprawdzenie_kroku2()
       cy.log('Wprowadzanie niepoprawnego imienia');
         cy.get('#checkout-addresses-step input[name="firstname"]').clear().type('1234');
         cy.get('input[name="address1"]').type('Testowa 12');
         cy.get('input[name="postcode"]').type('1234');
         cy.get('input[name="city"]').type('Test');
         cy.get('input[name="phone"]').type('123123123');
-        cy.button2_continue()    // funkcja w commands
+        cy.button2_continue()
       cy.log('Sprawdzenie, czy pojawia się błąd');
         cy.get('#checkout-addresses-step .alert-form') 
           .should('be.visible') 
@@ -127,18 +114,18 @@ const translations = {
       });
   
     it('Niepoprawne nazwisko w adresie (cyfry)', () => {
-        cy.poprawne_dane1krok()   // funkcja w commands
-        cy.checkboxy()    // funkcja w commands
-        cy.button1_continue()    // funkcja w commands
+        cy.poprawne_dane1krok()
+        cy.checkboxy()
+        cy.button1_continue()
       cy.log('Sprawdzenie, czy przeszło do kroku "Adresy"');
-      cy.sprawdzenie_kroku2()   // funkcja w commands
+      cy.sprawdzenie_kroku2()
       cy.log('Wprowadzanie niepoprawnego nazwiska');
         cy.get('#checkout-addresses-step input[name="lastname"]').clear().type('1234');
         cy.get('input[name="address1"]').type('Testowa 12');
         cy.get('input[name="postcode"]').type('1234');
         cy.get('input[name="city"]').type('Test');
         cy.get('input[name="phone"]').type('123123123');
-        cy.button2_continue()    // funkcja w commands
+        cy.button2_continue()
       cy.log('Sprawdzenie, czy pojawia się błąd');
         cy.get('#checkout-addresses-step .alert-form') 
           .should('be.visible') 
@@ -146,51 +133,50 @@ const translations = {
       });
   
     it('Niepoprawny numer telefonu', () => {
-        cy.poprawne_dane1krok()   // funkcja w commands
-        cy.checkboxy()    // funkcja w commands
-        cy.button1_continue()    // funkcja w commands
+        cy.poprawne_dane1krok()
+        cy.checkboxy()
+        cy.button1_continue()
       cy.log('Sprawdzenie, czy przeszło do kroku "Adresy"');
-      cy.sprawdzenie_kroku2()   // funkcja w commands
+      cy.sprawdzenie_kroku2()
       cy.log('Wprowadzanie niepoprawnego numeru telefonu');
         cy.get('input[name="address1"]').type('Testowa 12');
         cy.get('input[name="postcode"]').type('1234');
         cy.get('input[name="city"]').type('Test');
         cy.get('input[name="phone"]').type('abcd');
-        cy.button2_continue()    // funkcja w commands
+        cy.button2_continue()
       cy.log('Sprawdzenie, czy pojawia się błąd');
-      cy.sprawdzenie_error2krok()   // funkcja w commands
+      cy.sprawdzenie_error2krok()
       });
   
     it('Niepoprawny kod pocztowy (np. @)', () => {
-        cy.poprawne_dane1krok()   // funkcja w commands
-        cy.checkboxy()    // funkcja w commands
-        cy.button1_continue()    // funkcja w commands
+        cy.poprawne_dane1krok()
+        cy.checkboxy()
+        cy.button1_continue()
         cy.log('Sprawdzenie, czy przeszło do kroku "Adresy"');
-        cy.sprawdzenie_kroku2()   // funkcja w commands
+        cy.sprawdzenie_kroku2()
       cy.log('Wprowadzanie niepoprawnego kodu pocztowego');
         cy.get('input[name="address1"]').type('Testowa 12');
         cy.get('input[name="postcode"]').type('@');
         cy.get('input[name="city"]').type('Test');
         cy.get('input[name="phone"]').type('123123123');
-        cy.button2_continue()    // funkcja w commands
+        cy.button2_continue()
         cy.log('Sprawdzenie, czy pojawia się błąd');
-        cy.sprawdzenie_error2krok()   // funkcja w commands
+        cy.sprawdzenie_error2krok()
       });
   
     it('Niepoprawne miasto (np. @@@)', () => {
-        cy.poprawne_dane1krok()   // funkcja w commands
-        cy.checkboxy()    // funkcja w commands
-        cy.button1_continue()    // funkcja w commands
+        cy.poprawne_dane1krok()
+        cy.checkboxy()
+        cy.button1_continue()
       cy.log('Sprawdzenie, czy przeszło do kroku "Adresy"');
-      cy.sprawdzenie_kroku2()   // funkcja w commands
+      cy.sprawdzenie_kroku2()
       cy.log('Wprowadzanie niepoprawnej nazwy miasta');
         cy.get('input[name="address1"]').type('Testowa 12');
         cy.get('input[name="postcode"]').type('12');
         cy.get('input[name="city"]').type('@@@');
         cy.get('input[name="phone"]').type('123123123');
-        cy.button2_continue()    // funkcja w commands
+        cy.button2_continue()
       cy.log('Sprawdzenie, czy pojawia się błąd');
-      cy.sprawdzenie_error2krok()   // funkcja w commands
+      cy.sprawdzenie_error2krok()
       });
-  
   })  
